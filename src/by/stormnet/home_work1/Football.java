dafed1b3e3e5dc6b45395e709ac6202592ab51a1
Anastasia-Kruk <anastasiakruk1985@gmail.com>
2019-04-22 00:55:46 +0300

    Initial commit

---
+++ src/by/stormnet/home_work1/Football.java
@@ -0,0 +1,8 @@
+package by.stormnet.home_work1;
+
+class Ball {
+    public static void main (String [] args) {
+        System.out.println("Barcelona is champion!");
+    }
+
+}
---
+++ src/by/stormnet/home_work1/Spring.java
@@ -0,0 +1,7 @@
+package by.stormnet.home_work1;
+
+public class Spring {
+    public static void main (String[] args){
+        System.out.println("Spring is in Belarus now.");
+    }
+}
