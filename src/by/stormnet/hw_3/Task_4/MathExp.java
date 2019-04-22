e8d4b32ea16195be588d0dd46c59742001eae82e
Anastasia-Kruk <anastasiakruk1985@gmail.com>
2019-04-22 11:12:47 +0300

    Home work 3

--- src/by/stormnet/hw_3/Task_4/MathExp.java
+++ src/by/stormnet/hw_3/Task_4/MathExp.java
@@ -4,7 +4,7 @@ public class MathExp {
     public static void main(String[] args) {
         double degree = 180;
         double radians = Math.toRadians(180);
-        int cos = (int)Math.cos(radians);
+        double cos =Math.cos(radians);
 
 
         int num1 = 256;
@@ -12,7 +12,7 @@ public class MathExp {
         int num3 = 2;
         int num4 = 3;
 
-        double math = (cos + num1/num2 - num3 ^ num4)* Math.PI ;
+        double math = (cos + num1/num2 - Math.pow(num3,num4))* Math.PI ;
 
         System.out.println("Mathematical expression = " + math);
 
