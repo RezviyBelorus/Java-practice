81198a9728b5f3bcdd3ea4434d43760b7a713885
Aleksandr Fomin <RezviyBelorus@gmail.com>
2019-04-23 21:26:33 +0300

    Update Argument.java

--- src/by/stormnet/hw_3/Task_1/Argument.java
+++ src/by/stormnet/hw_3/Task_1/Argument.java
@@ -2,6 +2,9 @@ package by.stormnet.hw_3.Task_1;
 
 public class Argument {
     public static void main(String[] args) {
+        // !!! имена пакетов с маленькой буквы, т.е. Task_1 -> task_1
+        
+        // ты назвала переменную argument, по идее это уже результат, аргумент закончился на стадии парсинга его из строки
         int argument = Integer.parseInt(args[0]) << 8;
 
         System.out.println(argument);
